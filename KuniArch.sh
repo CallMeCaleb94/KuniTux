8d4b39ca684f1291672541e9987e4182d91ebd7e
CallMeCaleb94 <calebcodes94@gmail.com>
2023-05-22 22:42:07 -0400

    Initial Commit

---
+++ KuniArch.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+#Boots archlinux for user kunikos
+exec proot-distro login --user kunikos archlinux
---
+++ KuniBu.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+#Boot ubuntu for user kunikos
+exec proot-distro login --user kunikos ubuntu
