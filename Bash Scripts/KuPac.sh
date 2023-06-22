2e7865e1d6c5b710e58dfc612b249d05070b3425
CallMeCaleb94 <calebcodes94@gmail.com>
2023-06-21 20:42:41 -0400

    nvim

---
+++ Bash Scripts/KuPac.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+exec git clone https://github.com/KunikoKodes/pacman-for-termux;
+cd pacman-for-termux && bash install.sh;
+
+cd ..;
+exec mkdir usr-n/;
+cd usr-n/;
+exec wget "https://github.com/termux-pacman/termux-packages/releases/download/bootstrap-2023.06.18-r1%2Bpacman-android-7/bootstrap-x86_64.zip";
+exec unzip bootstrap-x86_64.zip
+exec cat SYMLINKS.txt | awk -F "←" '{system("ln -s '"'"'"$1"'"'"' '"'"'"$2"'"'"'")}'
+
